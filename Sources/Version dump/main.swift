88fd9df1f81f3eab2abfd21aa0e644d03adc434b
Damiaan Dufaux <damiaan@dpro.be>
2019-11-20 18:43:13 +0100

    Add commandline version dumper

---
+++ Sources/Version dump/main.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by Damiaan on 20/11/2019.
+//
+
+import Atem
+import Dispatch
+
+let address: String
+if CommandLine.arguments.count > 1 {
+	address = CommandLine.arguments[1]
+} else {
+	print("Enter switcher IP address: ", terminator: "")
+	address = readLine() ?? "192.168.8.149"
+}
+print("Trying to connect to switcher with IP addres", address)
+
+let controller = try Controller(ipAddress: address) { handler in
+	handler.when { (version: ProtocolVersion) in
+		print(version)
+	}
+
+	handler.when { (change: InitiationComplete) in
+		print("Initiation complete")
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+			exit(EXIT_SUCCESS)
+		}
+	}
+}
+
+dispatchMain()
+
+//import Foundation
+//
+//let messsages = try "[" + (2...18).map { index in
+//	let file = try Data(contentsOf: URL(fileURLWithPath: "/tmp/packet\(index)"))
+//	return "[" + file.map{"0x" + String($0, radix: 16)}.joined(separator: ", ") + "]"
+//}.joined(separator: ",\n") + "]"
+//
+//print(messsages)
