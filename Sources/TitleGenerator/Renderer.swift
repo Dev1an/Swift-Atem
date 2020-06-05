5042b8fe20472d3edf3ed90dde9b9ef18ab98a74
Damiaan Dufaux <damiaan@dpro.be>
2020-06-05 14:23:41 +0200

    Render in correct bitmap format
    encodeRunLength expects premultiplied 32-bit big endian RGBA (8-bit per channel).

--- Sources/TitleGenerator/Renderer.swift
+++ Sources/TitleGenerator/Renderer.swift
@@ -11,11 +11,17 @@ import MediaConverter
 @available(OSX 10.15, *)
 extension View {
 	func render() -> Data {
-		let frame = NSRect(x: 0, y: 0, width: 1920, height: 1080)
+		let width = 1920
+		let height = 1080
+		let frame = NSRect(x: 0, y: 0, width: width, height: height)
 		let view = NSHostingView(rootView: self)
 		view.frame = frame
-		let imageRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: 1920, pixelsHigh: 1080, bitsPerSample: 8, samplesPerPixel: 4, hasAlpha: true, isPlanar: false, colorSpaceName: .deviceRGB, bitmapFormat: .init(), bytesPerRow: 1920*4, bitsPerPixel: 32)!
+		let data = Data(count: width * height * 4)
+		let dataProvider = CGDataProvider(data: data as NSData)!
+		let cgImage = CGImage(width: width, height: height, bitsPerComponent: 8, bitsPerPixel: 32, bytesPerRow: width*4, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue), provider: dataProvider, decode: nil, shouldInterpolate: true, intent: .defaultIntent)!
+		let imageRep = NSBitmapImageRep(cgImage: cgImage)
 		view.cacheDisplay(in: frame, to: imageRep)
+//		try? imageRep.representation(using: .png, properties: [:])?.write(to: URL(fileURLWithPath: "/tmp/media.png"))
 		return encodeRunLength(data: imageRep.cgImage!.dataProvider!.data! as Data)
 	}
 }
