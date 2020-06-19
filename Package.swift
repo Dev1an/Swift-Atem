ed71efba134f5cca3c20a8456505345731630f32
Damiaan Dufaux <damiaan@dpro.be>
2020-06-19 09:19:40 +0200

    Use Swift-NIO 2.18.0 for Hashable SocketAddresses

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-		.package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+		.package(url: "https://github.com/apple/swift-nio.git", from: "2.18.0"),
 		.package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.5.1"),
     ],
     targets: [
