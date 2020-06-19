7b9a4db7e5f0a58d6b716cd1b81884d9a3ed4bc9
Damiaan Dufaux <damiaan@dpro.be>
2020-06-19 23:25:10 +0200

    don’t release controller object

--- Sources/SourceLabeler/main.swift
+++ Sources/SourceLabeler/main.swift
@@ -10,9 +10,16 @@ import Atem
 
 #if os(iOS) || os(watchOS) || os(tvOS) || os(macOS)
 if #available(OSX 10.15, *) {
-	let compressed = Label(text: "Media player 1").render()
-	try connect(ip: "10.1.0.210").uploadLabel(source: .mediaPlayer(0), labelImage: compressed)
-	dispatchMain()
+	let compressed = Label(text: "Preview 🎥").render()
+
+	let upload = DispatchGroup()
+	upload.enter()
+	let controller = try connect(ip: "10.1.0.210")
+	controller.uploadLabel(source: .preview(me: 0), labelImage: compressed)
+	upload.wait()
+
+	print(controller)
+
 } else {
 	// Fallback on earlier versions
 }
