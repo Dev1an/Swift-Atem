e28d15604dd17d42440cbaeb462964242fec27e3
Damiaan Dufaux <damiaan@dpro.be>
2020-06-19 19:57:37 +0200

    Disable swiftUI label on Linux

--- Sources/SourceLabeler/main.swift
+++ Sources/SourceLabeler/main.swift
@@ -9,7 +9,11 @@ import Foundation
 import Atem
 
 if #available(OSX 10.15, *) {
+	#if os(iOS) || os(watchOS) || os(tvOS) || os(macOS)
 	let compressed = Label(text: "Media player 1").render()
+	#else
+	let compressed = fatalError("Cannot render SwiftUI label on Linux")
+	#endif
 	try connect(ip: "10.1.0.210").uploadLabel(source: .mediaPlayer(0), labelImage: compressed)
 	dispatchMain()
 } else {
