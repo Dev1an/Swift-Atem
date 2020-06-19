1b50128f3c0f89df534087f54bd49c3b0f7f1cc2
Damiaan Dufaux <damiaan@dpro.be>
2020-06-19 20:01:38 +0200

    remove fatalError

--- Sources/SourceLabeler/main.swift
+++ Sources/SourceLabeler/main.swift
@@ -8,16 +8,14 @@
 import Foundation
 import Atem
 
+#if os(iOS) || os(watchOS) || os(tvOS) || os(macOS)
 if #available(OSX 10.15, *) {
-	#if os(iOS) || os(watchOS) || os(tvOS) || os(macOS)
 	let compressed = Label(text: "Media player 1").render()
-	#else
-	let compressed = fatalError("Cannot render SwiftUI label on Linux")
-	#endif
 	try connect(ip: "10.1.0.210").uploadLabel(source: .mediaPlayer(0), labelImage: compressed)
 	dispatchMain()
 } else {
 	// Fallback on earlier versions
 }
-
-
+#else
+	print("Cannot render label using SwiftUI")
+#endif
