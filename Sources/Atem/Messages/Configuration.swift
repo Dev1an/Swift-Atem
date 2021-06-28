729dffde987bd9510111d7b5fca09a7eeba084ca
Damiaan Dufaux <damiaan@dpro.be>
2021-06-28 12:27:09 +0200

    Publish ProtocolVesion and ProductInfo properties

--- Sources/Atem/Messages/Configuration.swift
+++ Sources/Atem/Messages/Configuration.swift
@@ -18,7 +18,7 @@ extension Message.Config {
 		public static let title = Message.Title(string: "_ver")
 		static let majorPosition = 0..<2
 		static let minorPosition = 2..<4
-		let minor, major: UInt16
+		public let minor, major: UInt16
 
 		public init(with bytes: ArraySlice<UInt8>) {
 			major = UInt16(from: bytes[ProtocolVersion.majorPosition.advanced(by: bytes.startIndex)])
@@ -44,9 +44,9 @@ extension Message.Config {
 		static let modelPosition = 40
 
 		/// The name of the product
-		let name: String
+		public let name: String
 		/// The model of the product
-		let model: Model
+		public let model: Model
 
 		public init(with bytes: ArraySlice<UInt8>) throws {
 			// Stores the string constructed from the first non-zero bytes
